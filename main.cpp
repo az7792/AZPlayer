d7d14502b43f52839c88544add7aac4184aff397
axf <1460014874@qq.com>
2025-08-24 00:28:16 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/AZPlayer/Main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
